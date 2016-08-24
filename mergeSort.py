5be01107d60ebaea4886e26d89a4a2706f6d9c1e
Tanya <tanyakovalenko1994@gmail.com>
2016-08-24 14:19:13 -0400

    Add merge and mergeSort functions to task #13.

--- mergeSort.py
+++ mergeSort.py
@@ -1,31 +1,54 @@
-print("Please, enter number of elements in the array")
-A = [0] * int(input())
-for inx in range(len(A)):
+print("Please, enter number of elements in the array AL")
+AL = [0] * int(input())
+for inx in range(len(AL)):
     val = int(input())
-    A[inx] = val
+    AL[inx] = val
 
-
-def merge(A):
-    l = 0
-    r = len(A) - 1
-    lm = (l + r)//2
-    rm = lm + 1
+print("Please, enter number of elements in the array AR")
+AR = [0] * int(input())
+for inx in range(len(AR)):
+    val = int(input())
+    AR[inx] = val
+    
+def merge(AL, AR):
     indx = 0
     inxL = 0
-    inxR = lm + 1
-    print("//////", len(A))
-    B = [0] * len(A)
-    for inx in range(len(A)):
-        if (inxL <= lm) or (inxR <= r):
-            if (inxR > r) or (A[inxL] < A[inxR]):                
-                B[indx] = A[inxL]
+    inxR = 0
+    B = [0] * (len(AL) + len(AR))
+    for inx in range(len(AL) + len(AR)):
+        if (inxL < len(AL)) or (inxR < len(AR)):
+            if (inxR >= len(AR)) or (AL[inxL] < AR[inxR]):                
+                B[indx] = AL[inxL]
                 print(B[indx])
                 inxL += 1
-            elif (inxL > lm) or (A[inxL] >= A[inxR]):
-                B[indx] = A[inxR]
+            elif (inxL >= len(AL)) or (AL[inxL] >= AR[inxR]):
+                B[indx] = AR[inxR]
                 print(B[indx])
                 inxR += 1
             indx += 1
     print(B)
+
+def mergeSort(A):
+    if len(A) == 2:
+        AL = A[0]
+        AR = A[1]
+        merge(AL, AR)
+    lAL = 0
+    rAR = len(A) - 1
+    rAL = (rAR + lAL) // 2
+    lAR = rAL + 1
+    inxL = 0
+    inxR = 0
+    while (lAL < rAL):
+        AL[inxL] = A[lAL]
+        inxL += 1
+        lAL += 1
+    while (lAR < rAR):
+        AR[inxR] = A[lAR]
+        inxR += 1
+        lAR += 1
         
-merge(A)
+    mergeSort(AL)
+    mergeSort(AR)
+    
+merge(AL, AR)
