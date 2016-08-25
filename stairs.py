0a9f0cf5ce678c2e985aea1aaa77d4cc23cc421e
Tanya <tanyakovalenko1994@gmail.com>
2016-08-25 13:06:55 -0400

    Fix after Ruslan's comment. Add function findNumOfWays

--- stairs.py
+++ stairs.py
@@ -1,26 +1,27 @@
-print("Please, enter the number of steps of the staircase")
-resultArr = [0] * int(input())
-print(len(resultArr))
+numOfStairs = [7, 8, 0, 15, 4]
+numOfSteps = [[2, 3, 4], [5, 2], [], [5, 4, 3], [1]]
+                
+def findNumOfWays(numOfStairs, numOfSteps):
+        for numOfStairsInx in range(len(numOfStairs)):
+                if (numOfStairs[numOfStairsInx] == 0):
+                        print("There are 0 variants of the ways to aproach the step .0")
+                elif (numOfStairs[numOfStairsInx] == 1):
+                        print("There are 0 variants of the ways to aproach the step .1")
+                else:
+                        resultArr = [0] * numOfStairs[numOfStairsInx]
+                        for inx in range(len(resultArr)):
+                                resultArr[inx] = 0
+                        for inx in range(len(resultArr)):
+                                jumps = numOfSteps[numOfStairsInx]
+                                for jumpsInx in range(len(jumps)):
+                                        if (inx == jumps[jumpsInx]):
+                                                resultArr[inx] += 1
+                                        if (inx > jumps[jumpsInx]):
+                                                if (resultArr[inx - jumps[jumpsInx]] != 0):
+                                                        resultArr[inx] += resultArr[inx - jumps[jumpsInx]]
+                        print("There are ", resultArr[len(resultArr)-1], " variants of the ways to aproach the step .", len(resultArr))
+        
 
-for inx in range(len(resultArr)):
-	resultArr[inx] = 0
-
-print("Please, enter count of possible values of number of steps in the jumps in ascending order")
-jumps = [0] * int(input())
-for inx in range(len(jumps)):
-    val = int(input())
-    jumps[inx] = val
-
-print(len(jumps))
-
-print("Len of the resultArr is ", len(resultArr))
-
-for inx in range(len(resultArr)):
-    for jumpsInx in range(len(jumps)):
-        if (inx == jumps[jumpsInx]):
-            resultArr[inx] += 1
-        if (inx > jumps[jumpsInx]):
-            if (resultArr[inx - jumps[jumpsInx]] != 0):
-                resultArr[inx] += resultArr[inx - jumps[jumpsInx]]
-
-print("There are ", resultArr[len(resultArr)-1], " variants of the ways to aproach the step .", len(resultArr))
+if __name__ == '__main__':
+        findNumOfWays(numOfStairs, numOfSteps)
+        pass
