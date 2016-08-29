0e17dd99a64b09042a19c91ce78b0ac9c7761d01
Tanya <tanyakovalenko1994@gmail.com>
2016-08-29 10:27:32 -0400

    Update mergeSort.py

--- mergeSort.py
+++ mergeSort.py
@@ -1,54 +1,55 @@
-print("Please, enter number of elements in the array AL")
-AL = [0] * int(input())
-for inx in range(len(AL)):
-    val = int(input())
-    AL[inx] = val
+import copy
 
-print("Please, enter number of elements in the array AR")
-AR = [0] * int(input())
-for inx in range(len(AR)):
-    val = int(input())
-    AR[inx] = val
+def merge(A, l, r):
     
-def merge(AL, AR):
-    indx = 0
-    inxL = 0
-    inxR = 0
-    B = [0] * (len(AL) + len(AR))
-    for inx in range(len(AL) + len(AR)):
-        if (inxL < len(AL)) or (inxR < len(AR)):
-            if (inxR >= len(AR)) or (AL[inxL] < AR[inxR]):                
-                B[indx] = AL[inxL]
-                print(B[indx])
-                inxL += 1
-            elif (inxL >= len(AL)) or (AL[inxL] >= AR[inxR]):
-                B[indx] = AR[inxR]
-                print(B[indx])
-                inxR += 1
-            indx += 1
-    print(B)
+    m = int((l+r) // 2)
+    inxL = l
+    inxR = m + 1
+    inx = l
+    B = copy.deepcopy(A)
 
-def mergeSort(A):
-    if len(A) == 2:
-        AL = A[0]
-        AR = A[1]
-        merge(AL, AR)
-    lAL = 0
-    rAR = len(A) - 1
-    rAL = (rAR + lAL) // 2
-    lAR = rAL + 1
-    inxL = 0
-    inxR = 0
-    while (lAL < rAL):
-        AL[inxL] = A[lAL]
-        inxL += 1
-        lAL += 1
-    while (lAR < rAR):
-        AR[inxR] = A[lAR]
-        inxR += 1
-        lAR += 1
+    while (inxL <= m) and (inxR <= r):
         
-    mergeSort(AL)
-    mergeSort(AR)
+        if A[inxL] < A[inxR]:
+            B[inx] = A[inxL]        
+            inxL += 1
+        else:
+            B[inx] = A[inxR]      
+            inxR += 1
+            
+        inx += 1
+        
+    while inxL <= m:
+        B[inx] = A[inxL]
+        inxL += 1
+        inx += 1
+        
+    while inxR <= r:
+        B[inx] = A[inxR]
+        inxR += 1
+        inx += 1
+
+    for indx in range(len(A)):
+        A[indx] = B[indx]
+
+def mergeSort(A, l, r):
     
-merge(AL, AR)
+    m = int((l+r)//2)
+    
+    if l < m:
+        mergeSort(A, l, m)
+        
+    if m+1 < r:
+        mergeSort(A, m+1, r)
+        
+    merge(A, l, r)
+
+    return A
+
+
+if __name__ == '__main__':
+        assert mergeSort([5, 9, 3, 2, 4, 15, 0], 0, 6) == [0, 2, 3, 4, 5, 9, 15]
+        assert mergeSort([7, 9, 3, 5], 0, 3) == [3, 5, 7, 9]
+
+
+
